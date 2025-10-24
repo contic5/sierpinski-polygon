4f390444fc890bcf6f91153c35dc2fe5bcc77dc2
CJ Conti <cjc2818cjc@gmail.com>
2025-10-24 10:30:35 -0400

    Added the ability to only draw certain depths. This allows for more varied drawings.

--- src/main.js
+++ src/main.js
@@ -30,8 +30,14 @@ class Polygon
 
   draw()
   {
+
+    //Only draw polygons if they are at least a certain depth.
+    if(this.depth<min_draw_depth)
+    {
+      return;
+    }
     //this.print_points();
-    ctx.fillStyle=colors[this.depth];
+    ctx.fillStyle=colors[this.depth-1];
     
     //Create a new Path2D that uses the points in the polygon. Start at the first point and then travel to each point.
     let region = new Path2D();
@@ -49,7 +55,7 @@ class Polygon
 }
 
 
-function create_polygon(center,radius,depth=0)
+function create_polygon_from_center(center,radius,depth=0)
 {
 
   //Generate points for a polygon
@@ -67,7 +73,7 @@ function create_polygon(center,radius,depth=0)
   polygons.push(new Polygon(center,points,depth));
 
   //Only generate smaller polygons if depth is less than or equal to 2.
-  if(depth<=2)
+  if(depth<max_depth)
   {
     generate_smaller_polygons(center,radius,depth);
   }
@@ -91,19 +97,24 @@ function generate_smaller_polygons(center,radius,depth)
   //console.log(scale_factor);
 
   /*
+  Generate smaller polygons.
   https://larryriddle.agnesscott.org/ifs/pentagon/ngondet.htm
   Each central angle is equal to 2*pi/n, so if w denotes the distance from the center to the vertex, then the vertices are at the points (w*cos(2 pi*k/n), w*sin(2 pi*k/n) for k = 1 to n. Therefore, the translation for the kth function is
   */
 
+  let centers=[];
   for(let k=1;k<=sides;k++)
   {
     let center_x=(1-scale_factor)*radius*Math.cos((2*Math.PI*k)/sides+offset_radians)+center[0];
     let center_y=(1-scale_factor)*radius*Math.sin((2*Math.PI*k)/sides+offset_radians)+center[1];
+    centers.push([center_x,center_y]);
 
     //console.log(x+" "+y);
     //Turn the center points into a polygon. Increase the depth by 1 to prevent infinite recursion.
-    create_polygon([center_x,center_y],scale_factor*radius,depth+1);
+    create_polygon_from_center([center_x,center_y],scale_factor*radius,depth+1);
   }
+
+
 }
 
 //This function centers the largest polygon.
@@ -137,7 +148,7 @@ function clear()
 function draw()
 {
   clear();
-  create_polygon([c.width/2,c.height/2],original_radius);
+  create_polygon_from_center([c.width/2,c.height/2],original_radius);
 
   //Calculate the offset
   const offset_y=calculate_offset();
@@ -171,6 +182,16 @@ export function setup()
   sides=parseInt(document.getElementById("sides").value);
   document.getElementById("sides_value").innerHTML=sides;
 
+  max_depth=parseInt(document.getElementById("max_depth").value);
+  document.getElementById("max_depth_value").innerHTML=max_depth;
+
+  min_draw_depth=parseInt(document.getElementById("min_draw_depth").value);
+  //Make sure that the min draw depth is not greater than the max depth.
+
+  min_draw_depth=Math.min(max_depth,min_draw_depth);
+  document.getElementById("min_draw_depth").value=min_draw_depth;
+  document.getElementById("min_draw_depth_value").innerHTML=min_draw_depth;
+
   //Rotate the polygon if it has an odd number of sides.
   offset_degrees=0;
   if(sides%2==1)
@@ -182,14 +203,20 @@ export function setup()
 }
 
 //Colors for the pattern
-let colors=["#ff0000","#ff8c00","#ffff00","#11ff00ff"];
-colors=["#6f88f7ff","#5a6ec8ff","#3e4d91ff","#262f59ff"];
+let colors=["#ff0000","#ff8c00","#ffff00","#11ff00ff","#0088ffff"];
+//colors=["#6f88f7ff","#5a6ec8ff","#3e4d91ff","#262f59ff"];
 let c=document.getElementById("my_canvas");
 let ctx=c.getContext("2d");
 
 let original_radius=c.width/2;
 //How many sides the polygon has
 let sides=3;
+
+//The maximum depth for generating polygons.
+let max_depth=4;
+
+//The minimum depth for drawing polygons.
+let min_draw_depth=4;
 
 let offset_degrees=0;
 let offset_radians=0;
